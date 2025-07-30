9b669ae4a4a734c97262beda8983e851f0361c6b
kraer <kraer@yandex.ru>
2025-07-30 20:41:40 +0300

    initial upload

---
+++ Homework/Rename.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+
+def rename_file(filename):
+
+    ctime = os.path.getctime(filename)
+    date_str = datetime.datetime.fromtimestamp(ctime).strftime("%Y-%m-%d")
+
+    name, ext = os.path.splitext(filename)
+    new_name = f"{name}_{date_str}{ext}"
+
+    os.rename(filename, new_name)
+    print(f"Файл {filename} переименован в {new_name}")
